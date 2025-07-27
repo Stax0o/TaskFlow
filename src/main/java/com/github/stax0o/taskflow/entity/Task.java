b7aa2821ec34c7f7a63e64ea945dcfe2dc7b963f
Artem Aliev <stax0o.devr@gmail.com>
2025-07-27 18:10:42 +0500

    feat: entities created

---
+++ src/main/java/com/github/stax0o/taskflow/entity/Task.java
@@ -0,0 +1,45 @@
+package com.github.stax0o.taskflow.entity;
+
+import com.github.stax0o.taskflow.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    private LocalDateTime deadline;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/github/stax0o/taskflow/enums/Status.java
@@ -0,0 +1,5 @@
+package com.github.stax0o.taskflow.enums;
+
+public enum Status {
+    TODO, IN_PROGRESS, DONE
+}
