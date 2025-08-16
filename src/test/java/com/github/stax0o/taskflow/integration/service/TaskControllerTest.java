55b4ca5bf10e0a042ab2f8a7d944d5c761b1fd01
Artem Aliev <stax0o.devr@gmail.com>
2025-08-16 13:35:08 +0500

    feat: add integration test

---
+++ src/test/java/com/github/stax0o/taskflow/integration/service/TaskControllerTest.java
@@ -0,0 +1,72 @@
+package com.github.stax0o.taskflow.integration.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.stax0o.taskflow.dto.TaskDTO;
+import com.github.stax0o.taskflow.entity.User;
+import com.github.stax0o.taskflow.enums.Status;
+import com.github.stax0o.taskflow.integration.AbstractIntegrationTest;
+import com.github.stax0o.taskflow.repository.TaskRepository;
+import com.github.stax0o.taskflow.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class TaskControllerTest extends AbstractIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final TaskDTO simpleTask = new TaskDTO(
+            null,
+            "Title",
+            "Description",
+            Status.TODO,
+            LocalDateTime.now().plusDays(3),
+            1L);
+
+    @BeforeEach
+    void createUser(){
+        User user = new User();
+        user.setUsername("testUser");
+        user.setEmail("test@gmail.com");
+
+        user = userRepository.save(user);
+    }
+
+    @Test
+    @DisplayName("POST /api/tasks - создание задачи")
+    void create_shouldCreatedTask() throws Exception {
+        mockMvc.perform(post("/api/tasks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(simpleTask)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.title").value(simpleTask.title()))
+                .andExpect(jsonPath("$.description").value(simpleTask.description()));
+
+        assertEquals(1, taskRepository.count());
+    }
+
+}
