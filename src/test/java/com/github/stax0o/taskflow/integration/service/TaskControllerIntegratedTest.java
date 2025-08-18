3d34f7eec0382f359e76d67f4d471a0cad7aae61
Artem Aliev <stax0o.devr@gmail.com>
2025-08-18 18:06:23 +0500

    feat: add integration test

--- src/test/java/com/github/stax0o/taskflow/integration/service/TaskControllerIntegratedTest.java
+++ src/test/java/com/github/stax0o/taskflow/integration/service/TaskControllerIntegratedTest.java
@@ -2,11 +2,13 @@ package com.github.stax0o.taskflow.integration.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.stax0o.taskflow.dto.TaskDTO;
+import com.github.stax0o.taskflow.entity.Task;
 import com.github.stax0o.taskflow.entity.User;
 import com.github.stax0o.taskflow.enums.Status;
 import com.github.stax0o.taskflow.integration.AbstractIntegrationTest;
 import com.github.stax0o.taskflow.repository.TaskRepository;
 import com.github.stax0o.taskflow.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,21 +21,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-class TaskControllerTest extends AbstractIntegrationTest {
+class TaskControllerIntegratedTest extends AbstractIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private TaskRepository taskRepository;
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -46,13 +46,23 @@ class TaskControllerTest extends AbstractIntegrationTest {
             LocalDateTime.now().plusDays(3),
             1L);
 
-    @BeforeEach
-    void createUser(){
+    @BeforeAll
+    static void createUser(
+            @Autowired UserRepository userRepository,
+            @Autowired TaskRepository taskRepository
+    ) {
         User user = new User();
         user.setUsername("testUser");
         user.setEmail("test@gmail.com");
-
         user = userRepository.save(user);
+
+        Task task = new Task();
+        task.setTitle("Title");
+        task.setDescription("Description");
+        task.setStatus(Status.TODO);
+        task.setDeadline(LocalDateTime.now().plusDays(3));
+        task.setUser(user);
+        taskRepository.save(task);
     }
 
     @Test
@@ -62,11 +72,24 @@ class TaskControllerTest extends AbstractIntegrationTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(simpleTask)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.id").value(2))
                 .andExpect(jsonPath("$.title").value(simpleTask.title()))
                 .andExpect(jsonPath("$.description").value(simpleTask.description()));
 
-        assertEquals(1, taskRepository.count());
+        assertEquals(2, taskRepository.count());
     }
+
+    @Test
+    @DisplayName("GET /api/tasks - Запрос задачи")
+    void getTaskById() throws Exception {
+        mockMvc.perform(get("/api/tasks/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.title").value(simpleTask.title()))
+                .andExpect(jsonPath("$.description").value(simpleTask.description()))
+                .andExpect(jsonPath("$.status").value(simpleTask.status().name()));
+    }
+
+
 
 }
